0da86a04172095f9281467e272ef3b03edb55bc1
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2025-06-15 18:21:10 +0900

    Update
    첫 실행 페이지, 스플래쉬 페이지 제작

--- bamtol_market_app/lib/main.dart
+++ bamtol_market_app/lib/main.dart
@@ -3,12 +3,14 @@ import 'package:bamtol_market_app/pages/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+late SharedPreferences prefs;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  prefs = await SharedPreferences.getInstance();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const MyApp());
 }
 
--- bamtol_market_app/lib/pages/app.dart
+++ bamtol_market_app/lib/pages/app.dart
@@ -1,12 +1,37 @@
+import 'package:bamtol_market_app/main.dart';
+import 'package:bamtol_market_app/pages/init_start_page.dart';
+import 'package:bamtol_market_app/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return _App();
+  }
+}
+
+class _App extends State<App> {
+  late bool isInitStarted;
+
+  @override
+  void initState() {
+    super.initState();
+    isInitStarted = prefs.getBool("isInitStarted") ?? true;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Image.asset('assets/images/logo_simbol.png')),
-    );
+    return isInitStarted
+        ? InitStartPage(
+            onStart: () {
+              setState(() {
+                isInitStarted = false;
+              });
+              prefs.setBool('isInitStarted', isInitStarted);
+            },
+          )
+        : const SplashPage();
   }
 }
