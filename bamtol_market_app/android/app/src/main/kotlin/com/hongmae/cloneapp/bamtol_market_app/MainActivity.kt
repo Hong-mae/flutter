106df8dbbaa0f1b826d2ee9aff9424af9a56990e
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2025-06-16 07:06:25 +0900

    Delete prj

--- bamtol_market_app/android/app/src/main/kotlin/com/hongmae/cloneapp/bamtol_market_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.hongmae.cloneapp.bamtol_market_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- bamtol_market_app/lib/consts/step_type.dart
+++
@@ -1,8 +0,0 @@
-enum StepType {
-  init(''),
-  dataLoad('데이터 로드'),
-  authCheck('인증 체크');
-
-  const StepType(this.label);
-  final String label;
-}
